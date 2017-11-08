a74fb96329e75e63be827ac17b038636c0e45023
Jessy Artnic <postcardhunter17@gmail.com>
2017-11-08 14:05:26 -0200

    Quelques unes modifications du front-end

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ var express = require('express'),
     app = express(),
     http = require('http').createServer(app),
     routes = require('./routes'),
-    io = require("socket.io")(http);
+    io = require("socket.io")(http),
+    users = new Array();
     
 app.set('view engine', 'pug');
 app.use("/public", express.static("public"));
@@ -12,12 +13,25 @@ routes(app);
 
 io.on("connection", (socket) => {
     // Fonction de submission à les autres utilisateurs
+    users.push({ id: socket.id, nick: socket.id});
+    io.emit("chat message", "Usuário conectado");
+
     socket.on("chat message", (msg) => {
+        // Envoyer des messages à les autres utilisateurs sauf le expéditeur
         socket.broadcast.emit("chat message", msg);
+    });
+
+    socket.on("set nick", (nickname) => {
+        users.forEach((elem) =>{
+            if(elem.id == socket.id)
+                elem.nick = nickname;
+        });
+        io.emit("chat message", "Nick alterado para " + nickname);
+        io.emit("users connected", users);
     });
 });
 
 http.listen(3000, () => {
     let D = new Date();
-    console.log("Server started at " + D.getMilliseconds());
-})
+    console.log("Server started at " + D.toString());
+});
--- public/js/main.min.js
+++ public/js/main.min.js
@@ -2,13 +2,19 @@ var appendMsg = (msg) => {
     $("#message-panel").append($("<li>").text(msg));
 };
 
+$("#command-line .btn").on("click", () =>{
+    appendMsg($("#message-text").val());
+    socket.emit("chat message", $("#message-text").val());
+});
+
 $(document).ready(() => {
     var socket = io();
-
-    $("#command-line .btn").on("click", () =>{
-        appendMsg($("#message-text").val());
-        socket.emit("chat message", $("#message-text").val());
-    });
-
+    socket.emit("set nick", "User " + new Date().getMilliseconds());
     socket.on("chat message", appendMsg);
+    socket.on("users connected", (list) => {
+        $("#user-list").text("");
+        list.forEach((elem) => {
+            $("#user-list").append($("<li>").text(elem.nick));
+        });
+    })
 });
